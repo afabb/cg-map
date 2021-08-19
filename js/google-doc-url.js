6e6cd45abab7d7d5af512829a7380a3b7709e314
afabb <82644074+afabb@users.noreply.github.com>
2021-08-19 15:29:44 -0700

    Update google-doc-url.js

--- js/google-doc-url.js
+++ js/google-doc-url.js
@@ -1,6 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SKsvz26coH35K1jicWxSzQA2aOSrsK60jE9wgTtx8ho/edit#gid=0';
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1PDvnXJ_JwdhlKu0B_62J_4Vln1qY4UDJPxSgJqg6G1A/edit#gid=0';
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/12N1WqVpjVjGzWf34ibDI18h1rsqhiahP5bxVN4HsCcc/edit#gid=0';
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-edRU1L5Qj00D6XcrxbnvvftOpQpIdb3CpYwII2F1uA/edit#gid=0';
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kUEElsp6AOk6Q4pgF9Q5c8MplYDfic5GrhYSBq8HPRI/edit#gid=0';
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kUEElsp6AOk6Q4pgF9Q5c8MplYDfic5GrhYSBq8HPRI/edit#gid=1330697361';
